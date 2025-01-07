0b1181be6cd4d2cb20eae9d50ffec991f5854a0d
abhishekchoudhary <abhishekchoudhary162004@gmail.com>
2025-01-07 14:48:19 +0530

    TodoList Completed with Context API

--- 7TodoList/src/context/TodoContext.js
+++ 7TodoList/src/context/TodoContext.js
@@ -1,25 +1,22 @@
-
-import {createContext, useContext} from 'react';
+import {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
-    todos: [{
-        id:1,
-        todo : "Todo 1",
-        completed:false
-    }],
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
     addTodo: (todo) => {},
-    updatedTodo: (id,todo) => {},
+    updateTodo: (id, todo) => {},
     deleteTodo: (id) => {},
     toggleComplete: (id) => {}
 })
 
 
-
-// Ye bs ek hook hai jo TodoContext ka use krta hai or TodoContext ka data return krta hai,
-// isse hume TodoContext ke har ek Value ko use karne ke liye direct use kar sakte bs isse import karke 
 export const useTodo = () => {
-    return useContext(TodoContext);
+    return useContext(TodoContext)
 }
 
-export const TodoProvider = TodoContext.Provider;
-
+export const TodoProvider = TodoContext.Provider
