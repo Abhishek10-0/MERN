b3f7ec137e14aeee6fbae1f4113a93c41d5e1f93
abhishekchoudhary <abhishekchoudhary162004@gmail.com>
2025-01-18 08:53:50 +0530

    compozent task

---
+++ 10reduxFlavoroProject/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='flex flex-col lg:flex-row justify-between py-8 px-12'>
+        <div >
+        {/* new Date().toUTCString().slice(0,16)   :- Ye line Date ke liye use kar rhe hai Date.now() se value sahi form mai
+        nhi aarha tha isliye ye Syntax use kiye hai   */}
+            <h3 className='text-xl font-bold text-gray-600'
+            >{new Date().toUTCString().slice(0,16)}</h3>   
+            <h1 className='text-2xl font-bold'
+            >Flaboro Food's</h1>
+        </div>
+
+        <div>
+            <input type="search" 
+            name='search'
+            id=''
+            placeholder='Search Here'
+            autoComplete='off'
+            className='  border border-gray-600 p-3 text-sm rounded-3xl w-full lg:width-25vw'
+            />
+        
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ 10reduxFlavoroProject/src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+        helo
+    </div>
+  )
+}
+
+export default Error
