c28cbb071ba1f9b7f30b9a46279b0fef2baa9a71
virendra959 <virendra.mumbai2001@gmail.com>
2025-01-25 13:05:21 +0530

    Staring new Project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-4xl text-center bg-blue-500 text-white'>
+      Hello
+    </h1>
+    </>
+  )
+}
+
+export default App
