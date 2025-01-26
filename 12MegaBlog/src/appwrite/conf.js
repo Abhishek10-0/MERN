c142bfe93555ecf3e4d9495b9a29cad534978a7c
virendra959 <virendra.mumbai2001@gmail.com>
2025-01-26 12:51:33 +0530

    Half Project done

--- 12MegaBlog/src/appwrite/conf.js
+++ 12MegaBlog/src/appwrite/conf.js
@@ -17,22 +17,62 @@ export class Service{
         this.bucket = new Storages(this.client);
     }
 
-    async createPost({title,content, featuredImage , status , UserId }){
+    async createPost({title , slug, content, featuredImage , status , UserId }){
         try{
             return await this.database.createDocument(config.appwriteDatabaseId, config.appwriteCollectionId, ID.unique,
                 {
                     title,
+                    slug,
                     content,
                     featuredImage,
                     status,
                     UserId,
-                    createdAt: Date.now()
+                    // createdAt: Date.now()
                 })
 
         }catch(error){
-            throw error;
+            console.log("Appwrite service :: createPost :: error", error);
+        }
+
+    }
+
+
+    async updatePost(slug, {title , content, featuredImage , status }){
+
+        try {
+            return await this.database.updateDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug, {
+                title,
+                content,
+                featuredImage,
+                status
+            }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error", error);
+        }
+        }
+
+
+    async deletePost(slug){   
+        try {
+            await this.database.deleteDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug);
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error", error);
+            return false;
         }
     }
+
+
+    async getPost(slug){
+        try {
+            return await this.database.getDocument(config.appwriteDatabaseId, config.appwriteCollectionId, slug);
+        }catch(error){
+            console.log("Appwrite service :: getPost :: error", error);
+        }
+    }
+
+    
 }
 
 
